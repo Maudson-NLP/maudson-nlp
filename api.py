75d0a037e6f9e02b41ea7697ae5cd25e3e42ede3
ionox0 <ionox0@gmail.com>
2017-03-19 22:50:18 -0400

    nltk download

--- api.py
+++ api.py
@@ -83,8 +83,9 @@ def strtobool (val):
 
 if __name__ == '__main__':
     # Install nltk english stopwords
-    if os.environ.get("HEROKU"):
-        nltk.download('punkt', 'wordnet')
+    if os.environ.get('HEROKU'):
+        nltk.download('wordnet')
+        nltk.download('punkt')
 
     env_port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=env_port, debug=True)
